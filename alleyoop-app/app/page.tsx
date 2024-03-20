9168da82cb8057650e23a6b661f1f9c39766abf4
riqthedev <riqthedev@gmail.com>
2024-03-20 17:48:38 -0400

    Added Clerk auth provider

--- alleyoop-app/app/page.tsx
+++ alleyoop-app/app/page.tsx
@@ -1,49 +1,5 @@
-import * as React from "react"
+import react from "react"
 
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-
-export default function CardWithForm() {
-  return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Sign Up</CardTitle>
-        <CardDescription>Create Profile</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Ex: John Doe " />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Password</Label>
-              <Input id="password" placeholder="password" type="password"/>
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Login</Button>
-        <Button>Create Profile</Button>
-      </CardFooter>
-    </Card>
-  )
+export default function Home() {
+  return <h1>Alley Opp</h1>
 }
