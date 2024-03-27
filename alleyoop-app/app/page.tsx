7862d2c33c63d0fb4d161f17f347c2e46e68e53b
riqthedev <riqthedev@gmail.com>
2024-03-27 15:53:52 -0400

    started building navbar

--- alleyoop-app/app/page.tsx
+++ alleyoop-app/app/page.tsx
@@ -1,22 +1,60 @@
+
 "use client"
-import React from "react";
-import Mapbox from "@/components/ui/map";
-import "../index.css"
-import geoFindMe from "./lib/actions";
+
+import * as React from "react"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
 
 
 
-export default function Home() {
-
-
+export default function NavigationMenuDemo() {
     return (
         <div>
-            <div id="map">
-                <Mapbox/>
-            </div>
-            
+            <NavigationMenu>
+                <h1>Alleyoop</h1>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            About
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+
+                <NavigationMenuItem>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        Contact
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+
+
+                <NavigationMenuItem>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        Sign Up
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        Log in
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+            </NavigationMenu>
+
+
+
         </div>
-    );
+    )
 }
+
+
 
 
